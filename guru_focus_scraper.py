0d64f8056eb6e5e7cd76b74e718c0cf7abff7ff1
lognorman20 <logannormanss@gmail.com>
2021-12-04 19:41:07 -0800

    first commit

---
+++ guru_focus_scraper.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+# Allows you to launch/initiate a browser
+
+from selenium.webdriver.common.by import By
+
+# Allows you to search for things using specific parameters.
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Allows you to wait for a page to load.
+
+from selenium.webdriver.support import expected_conditions as EC
+
+# Specify what you are looking for on a specific page in order to determine that the webpage has loaded.
+
+from selenium.common.exceptions import TimeoutException
